bda206428d5e7edad0e44307125e790a9b957814
Edmundo Neto <edmundobop@gmail.com>
2021-11-28 19:51:08 -0300

    alteracoes 28/11/21

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -2,7 +2,18 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
+    items: 1,
+    autoplay:1,
+    smartSpeed: 1,
     responsive:{
-        
+        0:{
+           
+        },
+        600:{
+            
+        },
+        1000:{
+            
+        }
     }
 })
